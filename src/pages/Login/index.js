7a4ae53ac909dbe19189ebeae648bd6c8f28c180
luopeng <11639446+luo-xiaohei-2@user.noreply.gitee.com>
2023-11-08 15:43:35 +0800

    feat:submit

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,27 +1,42 @@
-import './index.scss'
-import { Card,Form,Input,Button } from 'antd'
-import logo from './logo.png'
+import "./index.scss";
+import { Card, Form, Input, Button } from "antd";
+import logo from "./logo.png";
 
 const Login = () => {
-    return (
-        <div className='login'>
-            <Card className='login-container'>
-                <img className='login-logo' src={logo} alt='' />
-                <Form>
-                    <Form.Item>
-                        <Input size='large' placeholder='请输入手机号' />
-                    </Form.Item>
-                    <Form.Item>
-                        <Input size='large' placeholder='请输入验证码' />
-                    </Form.Item>
-                    <Form.Item>
-                        <Button type='primary' htmlType='submit' size='large' block>
-                            登录
-                        </Button>
-                    </Form.Item>
-                </Form>
-            </Card>
-        </div>
-    )
-}
-export default Login
+  const onFinish = (formValue) => {
+    console.log(formValue);
+  };
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        <Form onFinish={onFinish} validateTrigger={["onBlur"]}>
+          <Form.Item
+            name="mobile"
+            rules={[
+              { required: true, message: "请输入手机号" },
+              {
+                pattern: /^1[3-9]\d{9}$/,
+                message: "手机号码格式不对",
+              },
+            ]}
+          >
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item
+            name="code"
+            rules={[{ required: true, message: "请输入验证码" }]}
+          >
+            <Input size="large" placeholder="请输入验证码" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
+};
+export default Login;
