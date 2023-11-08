843ba7d7dc3dea94b5761b44e973d20ddb0dae0a
luopeng <11639446+luo-xiaohei-2@user.noreply.gitee.com>
2023-11-08 11:29:08 +0800

    feat:new router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import router from './router';
+import { RouterProvider } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
