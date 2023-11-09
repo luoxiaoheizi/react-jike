f426d003fdaa7b13af29ac1c401c98d11f903f6a
luopeng <11639446+luo-xiaohei-2@user.noreply.gitee.com>
2023-11-09 17:17:29 +0800

    feat:home echarts

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,17 @@
+import { BarChart } from '@/components/BarChart'
+
 const Home = () => {
-    return <div>Home</div>
-  }
-  export default Home
+  return (
+    <div>
+      <BarChart
+        xData={['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']}
+        sData={[120, 200, 150, 80, 70, 110, 130]} />
+      <BarChart
+        xData={['Vue', 'React', 'Angular']}
+        sData={[200, 500, 100]}
+        style={{ width: '500px', height: '400px' }} />
+    </div >
+  )
+}
+
+export default Home
